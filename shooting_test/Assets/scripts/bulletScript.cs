12778c21ea31490ab2fc4b0fe6d0995b4df5047b
bax533 <szymon.kosu@gmail.com>
2020-03-16 15:06:46 +0100

    stan ruchu i strzelania + strzelanie

---
+++ shooting_test/Assets/scripts/bulletScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+    public float lifeTime = 3f;
+
+    float startTime;
+    void Awake()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - startTime > lifeTime)
+            Destroy(this.gameObject);
+    }
+
+    /*void OnCollisionEnter()
+    {
+
+        Destroy(this.gameObject);
+    }*/
+}
---
+++ shooting_test/Assets/scripts/cameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{
+    public Transform playerPos;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        this.transform.position = new Vector3(playerPos.position.x, transform.position.y, transform.position.z);
+    }
+}
