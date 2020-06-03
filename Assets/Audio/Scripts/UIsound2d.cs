d57d31455e6f1fb6c263b9ddfa4e9bafbdbd23fc
DESKTOP-G4BFDQN\StrAnger <dtraut8@gmail.com>
2020-06-03 22:45:30 +0200

    added stair, text and error sounds

---
+++ Assets/Audio/Scripts/UIsound2d.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIsound2d : ShortSoundAttachment
+{
+    // Start is called before the first frame update
+    AudioSource src;
+    void Start()
+    {
+        if(this.GetComponent<AudioSource>() != null)
+        {
+            this.gameObject.AddComponent<AudioSource>();
+        }
+        src = GetComponent<AudioSource>();
+        src.spatialBlend = 0.0f;
+    }
+
+}
