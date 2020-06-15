9379592f0b2311ff03fd64e6713a99e51123f38f
Unknown <muchamichu@interia.pl>
2020-06-15 21:36:39 +0200

    Floating texts changed into icons

--- Assets/Objects/Players/FloatingHintBehaviour.cs
+++ Assets/Objects/Players/FloatingHintBehaviour.cs
@@ -5,25 +5,41 @@ using UnityEngine.UI;
 
 public class FloatingHintBehaviour : MonoBehaviour
 {
-    public Text hintText;
+    public Image hint;
 
-    string [] possibleTexts = {"", string.Format("Press ENTER\nopen/close"), string.Format("Press SPACE\nunlock & open/close"),
-    string.Format("Press ENTER\nhide"), string.Format("Press ENTER\nleave"), string.Format("Press ENTER\ngo upstairs/downstairs"),
-    string.Format("Press SPACE\ngo upstairs/downstairs")};
+    public Sprite [] possibleSprites;
+    // public Text hintText;
+
+    // string [] possibleTexts = {"", string.Format("Press ENTER\nopen/close"), string.Format("Press SPACE\nunlock & open/close"),
+    // string.Format("Press ENTER\nhide"), string.Format("Press ENTER\nleave"), string.Format("Press ENTER\ngo upstairs/downstairs"),
+    // string.Format("Press SPACE\ngo upstairs/downstairs")};
 
     void Start ()
     {
-        setText(0);
+        // setText(0);
+        clearSprite ();
     }
 
-    public void setText (int textId)
+    public void clearSprite ()
     {
-        hintText.text = possibleTexts[textId];
+        Color c = hint.color;
+        c.a = 0.0f;
+        hint.color = c;
+        //hint.sprite = null;
+    }
+
+    public void setSprite (int spriteId)
+    {
+        Color c = hint.color;
+        c.a = 1.0f;
+        hint.color = c;
+        hint.sprite = possibleSprites[spriteId];
+        // hintText.text = possibleTexts[textId];
     }
 
     void Update()
     {
-        Vector3 hintPosition = Camera.main.WorldToScreenPoint(transform.position);
-        hintText.transform.position = hintPosition;
+        Vector3 hintPos = Camera.main.WorldToScreenPoint(transform.position);
+        hint.transform.position = hintPos;
     }
 }
