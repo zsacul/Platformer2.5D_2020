571cb7eb7d39573e3401342c9223e849ea25cbd8
Basia <barbara.anna.piasecka@gmail.com>
2020-06-28 16:14:02 +0200

    enemies + helicopter events

--- Assets/Objects/Reflector-Helicopter/ReflectorScript.cs
+++ Assets/Objects/Reflector-Helicopter/ReflectorScript.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ReflectorScript : MonoBehaviour
 {
+    public UnityEvent helicopterFlying;
     public float speed = 3f;
     void Start()
     {
-        
+        if(helicopterFlying != null)
+        {
+            helicopterFlying.Invoke();
+        }
     }
 
     // Update is called once per frame
