659a16f19dde46a1b08541bc0d35b7c906b1c259
DESKTOP-G4BFDQN\StrAnger <dtraut8@gmail.com>
2020-06-30 00:31:37 +0200

    added new sounds

---
+++ Assets/Audio/Scripts/EnemySoundsHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySoundsHandler : MonoBehaviour
+{
+    public void EnemyStep()
+    {
+        AudioManager.Instance.PlaySoundOnce(AudioManager.Instance.Sounds.sRunning, this.gameObject, AudioManager.Instance.Sounds.mAnimations);
+    }
+
+    public void EnemyClimb()
+    {
+        AudioManager.Instance.PlaySoundOnce(AudioManager.Instance.Sounds.sCrouchWalking, this.gameObject, AudioManager.Instance.Sounds.mAnimations);
+    }
+}
