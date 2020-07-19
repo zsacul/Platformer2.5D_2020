4a3e047a1c5176f56803d19c58f2fe12380acac9
Basia <barbara.anna.piasecka@gmail.com>
2020-07-19 20:00:00 +0200

    Start Screen

---
+++ Assets/Scripts/startScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startScreen : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(Wait());
+        //LoadFirstScene
+    }
+
+    public IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(8f);
+        //LoadFirstScene
+    }
+}
